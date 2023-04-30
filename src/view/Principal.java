71856a95a80a4a85cf1ea6dbdfa55c130cc17c2a
T450 <T450@Mari.mshome.net>
2023-04-30 15:48:39 -0300

    atualização

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -10,40 +10,24 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Lista l=new Lista();
-		Pilha p=new Pilha();
+	
 		l.isEmpty();
 		int[] vet = {3,5,18,12,9,7,6,2,13,4,16};
-		int t=0;
-	
-		for(int i =0; i<11;i++) {
-			//int v =vet[i];
-			p.isEmpty();
-			p.push(vet[i]);
-			
-			}
-		for(int i=0;i<11; i++) {
-		try {
-			if(p.isEmpty()) {
-				l.addFirst(p.pop());
+
+		//pensei em outra forma de resolver sem usar a pilha, espero que esteje certo
+		for (int i=0;i<vet.length;i++) {
+			if(i==0) {
+		l.addFirst(vet[vet.length-1]);
 			}else {
-				l.addLast(p.pop());
+				l.addFirst(vet[vet.length-(i+1)]);
 			}
-					
-			System.out.println("test " + l.get(i));
-			} catch (Exception e) {
-			e.printStackTrace();
+		
+		try {
+			System.out.print("  "+ l.get(0));
+		} catch (Exception e1) {
+			e1.printStackTrace();
 		}
-		
-		
-		//try {
-			//int v =p.pop();
-			//System.out.println(" deletado "+v);
-		//} catch (Exception e) {
-			// TODO Auto-generated catch block
-		//	e.printStackTrace();
-	//	}
-		}
-		
+			}
 		
 	}
 
